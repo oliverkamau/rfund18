145ae5fd30d336afc069db3c2b000e47b7ca5416
kamaugaby13 <kamaugaby13@gmail.com>
2024-08-27 23:18:38 +0300

    Changes to App

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,11 +3,31 @@ import styles from './styles/Todo.module.css';
 export default function Todo(){
      const [todo, setTodo] = useState({activity:"",done:false});
      const [todos, setTodos] = useState([]);
+     const [count, setCount] = useState(0);
+  
+    function handleDone(value){
+      console.log(value);
+      // let index = todos.indexOf(value);
+      // if (index > -1) { 
+      //   todos.splice(index, 1);
+      // }
+      // if(value.done){  
+      //   setTodos([...todos,{activity:value.activity,done:false}]);
+      // }else{
+      //   setTodos([...todos,{activity:value.activity,done:true}]);
+      // }
+      const newTodos = todos.map((myTodo) => (myTodo.activity === value.activity) ? ({...myTodo, done:!myTodo.done}) : myTodo);
+      setTodos(newTodos);
+ 
+   }
+  
      function handleSubmit(e){
         e.preventDefault();
+        let count = todos.length;
         console.log(todo);
         setTodos([...todos, todo]);
-        setTodo({activity:"",done:false});
+        setCount(count + 1);
+        setTodo({activity:"",done:true});
      }
      function handleRemove(value){
       let index = todos.indexOf(value);
@@ -21,7 +41,9 @@ if (index > -1) {
       todos.map((value, index) => (
       <tr key={index}>
       <td>
-          {value.activity}
+        {(value.done) ?
+          (<del className={styles.strike} onClick={() => (handleDone(value))}>{value.activity}</del>)
+          :  (<span className={styles.normal} onClick={() => (handleDone(value))}>{value.activity}</span>)}
       </td>
        <td className={styles.floatleft}>
        <button type="button" className={styles.removebutton} onClick={() => handleRemove(value)}></button>
@@ -32,7 +54,7 @@ if (index > -1) {
      }
     return (
         <div>
-          <h2>Welcome To Today's Calendar</h2>
+          <h2>Welcome To Today's Calendar {count} Activities</h2>
           <div  className={styles.pickdiv}>
         <form onSubmit={handleSubmit}>
             <input type="text" id="todoItem" onChange={(e) => setTodo({activity:e.target.value,done:false})} value={todo.activity}/>
